69395c0bde48f2594452db1e56fb4154e6bf0f81
Bing Pan <vctpan@gmail.com>
2020-11-07 14:27:55 -0500

    Adjusted the main() to also accept user's input of URLs via console in addition to the command line

--- httplinkchecker/src/httplinkchecker/LinkChecker.java
+++ httplinkchecker/src/httplinkchecker/LinkChecker.java
@@ -3,22 +3,53 @@ package httplinkchecker;
 //This class is the entry point of my tool
 
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LinkChecker {
 public static int MAXNUMPATHS = 10;
 	
 	public static void main(String[] args) {		
 		Message msg = Message.getInstance();
-			
+		
+		ArgParser ap = null;
+		ArrayList<String> urls = null;
+		ArrayList<File> files = null;
+		PageContentParser pcp = null;
+		
 		//.........................................................................
 		//0. CLI arguments for testing
 		//String[] cmdarg = new String[0];
 		//String[] cmdarg = {"-r", "-HElp", "sample.txt", "this is not a file name", "C:\\Users\\2water\\eclipse-workspace",  "C:\\Users\\2water\\eclipse-workspace\\t2.txt", "C:\\Users\\2water\\eclipse-workspace\\httplinkchecker\\index.html", "t1.txt"};
 		//String[] cmdarg = {"-v", "-HElp","t1.txt", "-r", "C:\\Users\\2water\\eclipse-workspace\\httplinkchecker\\index.html"};
-		String[] cmdarg = {"-v", "--good", "url-list"};
+		//String[] cmdarg = {"-v", "--good", "url-list"};
 		
+
+		if(args.length > 0) {
+			 ap = new ArgParser(cmdarg);
+		}else {
+			 urls = new ArrayList<>();
+			 Scanner urlInput = new Scanner(System.in);
+			 System.out.println("Please enter a URL");
+			 
+			 while (urlInput.hasNext()) {
+				 urls.add(urlInput.next());
+		        }
+		 
+			 urlInput.close();
+		}
+		
+		if(urls != null) {
+			pcp = new PageContentParser(urls);
+			files = new ArrayList<>();
+			files = pcp.getFiles();
+		}
+
+
+
 		//.........................................................................
 		//1. based on file names, and options, get all absolute paths to the files
-		 ArgParser ap = new ArgParser(args);
 		
 		  System.out.print("dirPaths: "); 
 		  msg.displyMessage("Element", 0, ap.getDirPaths(), null);
@@ -40,8 +71,8 @@ public static int MAXNUMPATHS = 10;
 		
 		//.........................................................................
 		//2. in each available file, search for links via regex
-		  LinkStringExtractor lr = new LinkStringExtractor(ap.getDirPaths(), ap.getFileNames(), ap.getOptions());
-		
+		//LinkStringExtractor lr = new LinkStringExtractor(ap.getDirPaths(), ap.getFileNames(), ap.getOptions());
+		LinkStringExtractor lr = new LinkStringExtractor(files);
 		
 		//.........................................................................
 		//3. validate the retrieved links via ExecutorsService
